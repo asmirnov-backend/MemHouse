5c20e0447b23db9cbf20f02d9f868e3bfacabae9
Andrew Smirnov <asmirnov@upteam.com>
2022-06-11 20:00:06 +0300

    Remove Federation

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,17 +1,14 @@
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
-import {
-  ApolloFederationDriver,
-  ApolloFederationDriverConfig,
-} from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
-      driver: ApolloFederationDriver,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       typePaths: ['./apps/api/src/user/user.graphql'],
       definitions: { path: 'apps/api/src/user/user.graphql.interface.ts' },
     }),
