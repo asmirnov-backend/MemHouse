8c04e105ad72037d2d9e42fb6035d986e2a202d9
Andrew Smirnov <asmirnov@upteam.com>
2022-06-11 20:00:31 +0300

    Remove Federation in mem module

--- apps/api/src/mem/mem.module.ts
+++ apps/api/src/mem/mem.module.ts
@@ -1,17 +1,14 @@
 import { MemResolver } from './mem.resolver';
 import { MemService } from './mem.service';
 
-import {
-  ApolloFederationDriver,
-  ApolloFederationDriverConfig,
-} from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
-      driver: ApolloFederationDriver,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       typePaths: ['./apps/api/src/mem/mem.graphql'],
       definitions: { path: 'apps/api/src/mem/mem.graphql.interface.ts' },
     }),
