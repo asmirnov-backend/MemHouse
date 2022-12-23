e967971e2d598760aba856319243976f62d79b7b
Andrew Smirnov <smirnov@brick.dev>
2022-12-23 19:30:15 +0300

    Add images to mems in seed

--- prisma/seed/mems.ts
+++ prisma/seed/mems.ts
@@ -9,57 +9,6 @@ export const seedMems: Mem[] = [
     createdUserId: seedUsers[0].id,
   },
   {
-    id: '19888a7d-ff42-42a5-95e5-1717675725a2',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1717675725a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1717675724a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1717675723a1',
-
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1717675624a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1717675524a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1717675424a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1715675424a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1725675424a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
-    id: '19888a7d-ff42-42a5-95e5-1725175424a1',
-    text: null,
-    createdUserId: seedUsers[0].id,
-  },
-  {
     id: '19888a7d-ff42-42a5-95e5-1725175724a1',
     text: null,
     createdUserId: seedUsers[0].id,
