42eb5fb4e30a112cefdd3e45c37f2111dd09d3c1
Andrew Smirnov <smirnov@brick.dev>
2023-03-04 20:24:15 +0300

    Use nest logger in bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,16 @@
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './logging.interceptor';
 
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const appContext = await NestFactory.create(ConfigModule.forRoot());
   const config = appContext.get(ConfigService);
-  const httpPort = Number(config.getOrThrow<string>('APP_PORT')) || 3000;
+  const port = Number(config.getOrThrow<string>('APP_PORT')) || 3000;
 
   // { cors: true } is needed to process requests from the internet
   const app = await NestFactory.create(AppModule, {
@@ -18,6 +19,7 @@ async function bootstrap() {
         'https://memhouse-client.web.app',
         'http://localhost:3000',
         'http://localhost:3001',
+        'http://127.0.0.1:3000',
         'https://localhost',
       ],
       credentials: true,
@@ -28,6 +30,7 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
-  await app.listen(httpPort);
+  await app.listen(port);
+  logger.log(`Nest is started on port ${port}`);
 }
 bootstrap();
