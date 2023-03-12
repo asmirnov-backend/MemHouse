6714b6e598d1deae6ae0f1345ac27da9179e2d72
Andrew Smirnov <smirnov@brick.dev>
2023-03-12 16:28:58 +0300

    Not use appContext to get config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,15 +2,12 @@ import { AppModule } from './app.module';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const appContext = await NestFactory.create(ConfigModule.forRoot());
-  const config = appContext.get(ConfigService);
-  const port = Number(config.getOrThrow<string>('APP_PORT')) || 3000;
 
   // { cors: true } is needed to process requests from the internet
   const app = await NestFactory.create(AppModule, {
@@ -25,6 +22,10 @@ async function bootstrap() {
       credentials: true,
     },
   });
+
+  const config = app.get(ConfigService);
+  const port = Number(config.getOrThrow<string>('APP_PORT')) || 3000;
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.use(json({ limit: '50mb' }));
